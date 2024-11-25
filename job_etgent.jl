55a95cdaf654d2fe184f2427366096a2cf56d10e
Terkadur <47321551+Terkadur@users.noreply.github.com>
2024-11-25 16:04:20 -0500

    attractive 8x8

--- job_etgent.jl
+++ job_etgent.jl
@@ -7,12 +7,12 @@ Random.seed!(seed)
 const Nₖ = 5
 const λₖ_list = collect(0.0:0.2:0.8)
 
-const Lx, Ly = 4, 4
+const Lx, Ly = 8, 8
 const T = hopping_matrix_Hubbard_2d(Lx, Ly, 1.0)
-const U = 8.0
+const U = -2.0
 @show U
 
-const N_up, N_dn = 8, 8 #parse(Int64, ARGS[2]), parse(Int64, ARGS[3])
+const N_up, N_dn = 16, 16
 
 const system = GenericHubbard(
     # (Nx, Ny), (N_up, N_dn)
@@ -22,8 +22,8 @@ const system = GenericHubbard(
     # μ
     0.0,
     # β, L
-    20.0, 200,
-    # data type of the system
+    50.0, 500,
+    # data type of the systems
     sys_type=ComplexF64,
     # if use charge decomposition
     useChargeHST=false,
@@ -34,11 +34,11 @@ const system = GenericHubbard(
 const qmc = QMC(
     system,
     # number of warm-ups, samples and measurement interval
-    512, 8192, 10,
+    512, 1024, 10,
     # stablization and update interval
     10, 10,
     # if force spin symmetry
-    forceSymmetry=false,
+    forceSymmetry=true,
     # debugging flag
     saveRatio=false
 )
@@ -48,16 +48,14 @@ const φ₀_up = trial_wf_free(system, 1, T)
 const φ₀_dn = trial_wf_free(system, 2, T)
 const φ₀ = [φ₀_up, φ₀_dn]
 
-const Aidx = collect(1:8)
+const Aidx = collect(1:16)
 const extsys = ExtendedSystem(system, Aidx, subsysOrdering=false)
 
-path = "./data/4x4/"
+path = "./data/8x8/asym"
 
 swap_period = 256
 
-for λₖ in λₖ_list
-    # 6617s (1h50m)
-    @show λₖ
-    filename = "etgent/EtgEnt_LA$(length(Aidx))_N$(sum(system.N))_U$(system.U)_lambda$(λₖ)_beta$(system.β)_seed$(seed).jld"
-    @time run_incremental_sampling_gs(extsys, qmc, φ₀, λₖ, Nₖ, path, filename, swap_period)
-end
+λₖ = parse(Float64, ARGS[2])
+@show λₖ
+filename = "etgent/EtgEnt_LA$(length(Aidx))_N$(sum(system.N))_U$(system.U)_lambda$(λₖ)_beta$(system.β)_seed$(seed).jld"
+@time run_incremental_sampling_gs(extsys, qmc, φ₀, λₖ, Nₖ, path, filename, swap_period)
