b90e8b624732ee07fd09e04ff6b859ae0cebf482
Terkadur <47321551+Terkadur@users.noreply.github.com>
2024-12-31 00:15:24 -0500

    fixed?

--- averaging_etgent.jl
+++ averaging_etgent.jl
@@ -12,10 +12,11 @@ S2_err = zeros(Float64, 1)
 S2_conv = Vector{Float64}[]
 
 # merge data
-filelist = [filter(x -> match(Regex("EtgEnt_LA2_Nup1_Ndn2_U2.0_lambda$(lambda)_beta12.0_*"), x) !== nothing, readdir(path_src)) for lambda in lambda_list]
+filelist = [filter(x -> match(Regex("EtgEnt_LA2_Nup1_Ndn2_U2.0_lambda$(lambda)_beta18.0_*"), x) !== nothing, readdir(path_src)) for lambda in lambda_list]
 if isempty(filelist[1])
     throw("no files found")
 end
+detgA_list = Vector{Float64}[]
 numer_list = Vector{Float64}[]
 denom_list = Vector{Float64}[]
 
@@ -27,11 +28,16 @@ for filenames in filelist
     sgnprob_data = vcat([data[i]["sgnprob"] for i in 1:length(filenames)]...)
     detgA_data = vcat([data[i]["sgndetgA"] .* data[i]["absdetgA"] for i in 1:length(filenames)]...)
 
+    push!(detgA_list, detgA_data)
     push!(numer_list, detgA_data .* sgnprob_data)
     push!(denom_list, sgnprob_data)
 end
 
 # stats
+detgA_avg = mean.(detgA_list)
+detgA_err = std.(detgA_list) ./ length(detgA_list)
+detgA = measurement.(detgA_avg, detgA_err)
+
 numer_avg = mean.(numer_list)
 numer_err = std.(numer_list) ./ length(numer_list)
 numer = measurement.(numer_avg, numer_err)
@@ -40,16 +46,19 @@ denom_avg = mean.(denom_list)
 denom_err = std.(denom_list) ./ length(denom_list)
 denom = measurement.(denom_avg, denom_err)
 
-ratio = numer ./ denom
-S2 = -log(prod(ratio))
+S2_unsigned = -log(prod(detgA))
+S2_signed = -log(prod(numer ./ denom))
 
 # detgA_avg = mean.(detgA_list)
 # detgA_err = std.(detgA_list) ./ length(detgA_list)
 # expmS2 = measurement.(detgA_avg, detgA_err)
 # S2 = -log(prod(expmS2))
 
-@show S2.val
-@show S2.err
+@show S2_unsigned.val
+@show S2_unsigned.err
+
+@show S2_signed.val
+@show S2_signed.err
 
 # convergence
 # sweeps = length(detgA_list[1])
@@ -62,15 +71,15 @@ S2 = -log(prod(ratio))
 # end
 # push!(S2_conv, vec(-log.(prod(detgA_run, dims=1))))
 
-sweeps = length(numer_list[1])
-@assert length(denom_list[1]) == sweeps "Unequal number of sweeps for numer and denom"
+# sweeps = length(numer_list[1])
+# @assert length(denom_list[1]) == sweeps "Unequal number of sweeps for numer and denom"
 
-numer_run = cumsum(numer_list[1]) ./ collect(1:sweeps)
-denom_run = cumsum(denom_list[1]) ./ collect(1:sweeps)
-push!(S2_conv, vec(-log.(abs.(numer_run ./ denom_run))))
+# numer_run = cumsum(numer_list[1]) ./ collect(1:sweeps)
+# denom_run = cumsum(denom_list[1]) ./ collect(1:sweeps)
+# push!(S2_conv, vec(-log.(abs.(numer_run ./ denom_run))))
 
-jldopen("$(path_dst)" * "EtgEnt_LA2_U2.0_beta12.0_Nk1.jld", "w") do file
-    # write(file, "filling", filling_list)
-    write(file, "S2_conv", S2_conv)
-end
+# jldopen("$(path_dst)" * "EtgEnt_Nup1_Ndn1_LA2_U2.0_beta10.0_Nk1.jld", "w") do file
+#     # write(file, "filling", filling_list)
+#     write(file, "S2_conv", S2_conv)
+# end
 
